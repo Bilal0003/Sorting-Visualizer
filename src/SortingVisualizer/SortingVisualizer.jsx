f480f6dd0a9674dda6794efdd9e0b6ffc678ba8f
Bilal0003 <hamdaoui21@gmail.com>
2023-12-07 01:05:05 +0100

    weqw

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { BubbleSort } from '../AlgorithmsTri/AlgorithmsTri.js';
+import { BubbleSort } from '../AlgorithmsTri/AlgorithmsTri.js';
 import './SortingVisualizer.css';
 import { render } from '@testing-library/react';
 import { toHaveFocus } from '@testing-library/jest-dom/matchers';
@@ -7,59 +7,69 @@ import { toHaveFocus } from '@testing-library/jest-dom/matchers';
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
         super(props);
-    
-    this.state = {
-        array: [],
-    };
+
+        this.state = {
+            array: [],
+        };
     }
 
-componentDidMount(){
-    this.resetArray();
-}
-
-resetArray(){
-    const array = [];
-    for (let i = 0; i < 300; i++) {
-        array.push(nbrAleatoire(10, 600));
+    componentDidMount() {
+        this.resetArray();
     }
-    this.setState({ array });
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 300; i++) {
+            array.push(nbrAleatoire(10, 600));
+        }
+        this.setState({ array });
+    }
+
+
+    BubbleSort() {
+
+        const mysortedArr = BubbleSort(this.state.array);
+        this.setState(mysortedArr);
+
+    }
+
+    render() {
+        const { array } = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div
+                        className="array-bar"
+                        key={idx}
+                        style={{ height: `${value}px` }}></div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generer un nouveau vecteur.</button>
+                <button class="btn" onClick={() => this.BubbleSort()}>
+                    <span class="inner">
+                        <span class="slide"></span>
+                        <span class="content"> Bubble Sort</span>
+                    </span>
+                </button>
+            </div>
+        );
+    }
 }
 
-
-BubbleSort() {
-    
-    const mysortedArr = BubbleSort(this.state.array);
-    
-    
-}
-
-render(){
-    const { array } = this.state;
-
-    return (
-        <div className="array-container">
-            {array.map((value, idx) => (
-                <div 
-                className="array-bar"
-                key={idx}
-                style={{ height: `${value}px` }}></div>
-            ))}
-            <button onClick={() => this.resetArray()}>Generer un nouveau vecteur.</button>
-            <button onClick={() => this.BubbleSort()}>Bubble Sort</button>
-        </div>
-    );
-}
-}
-
-function nbrAleatoire(min,max){
+function nbrAleatoire(min, max) {
     return Math.random() * (max - min) + min;
 };
 
 
-function comparerVect(arr1, arr2){
-    if(arr1.length !== arr2.length) return false;
-    for(let i=0; i<arr1.length; i++){
-        if(arr1[i] !== arr2[i]) return false;
+function comparerVect(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
     }
     return true;
+}
+
+function TestingSortedArray() {
+
+
 }
